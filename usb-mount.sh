95a64ead41bc280681364dd20509eda0428a6f79
Augusto Resende <augusto.resende@outlook.com>
2019-07-16 15:48:12 -0300

    Update usb-mount.sh

--- usb-mount.sh
+++ usb-mount.sh
@@ -38,23 +38,7 @@ do_mount()
         exit 1
     fi
 
-    # Get info for this drive: $ID_FS_LABEL and $ID_FS_TYPE
-    eval $(blkid -o udev ${DEVICE} | grep -i -e "ID_FS_LABEL" -e "ID_FS_TYPE")
-
-    # Figure out a mount point to use
-    LABEL=${ID_FS_LABEL}
-    if grep -q " /media/${LABEL} " /etc/mtab; then
-        # Already in use, make a unique one
-        LABEL+="-${DEVBASE}"
-    fi
-    DEV_LABEL="${LABEL}"
-
-    # Use the device name in case the drive doesn't have label
-    if [ -z ${DEV_LABEL} ]; then
-        DEV_LABEL="${DEVBASE}"
-    fi
-
-    MOUNT_POINT="/media/${DEV_LABEL}"
+    MOUNT_POINT="/media/usb"
 
     ${log} "Mount point: ${MOUNT_POINT}"
 
